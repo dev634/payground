c030342ec1432f3d5b851d1f92c63ffcaeedf6f3
Devidev <47260520+dev634@users.noreply.github.com>
2020-08-08 22:18:10 +0200

    sec commit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,7 +2,7 @@ function loading() {
 	const loader = document.getElementById("loader");
 	setTimeout(() => {
 		loader.classList.add("is-hidden");
-	}, 200);
+	}, 400);
 }
 
 window.addEventListener("load", loading);
