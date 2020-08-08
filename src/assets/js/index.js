6777dff80e93c40f713c75669a3306b7f1b381e2
Devidev <47260520+dev634@users.noreply.github.com>
2020-08-09 00:47:22 +0200

    sec commit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,19 @@
 function loading() {
-	const loader = document.getElementById("loader");
-	setTimeout(() => {
-		loader.classList.add("is-hidden");
-	}, 400);
+	const title = document.querySelector("main h2");
+
+	const path = window.location.pathname;
+	const page = path.split("/").pop();
+	switch (page) {
+		case "index.html":
+			const loader = document.getElementById("loader");
+			loader.classList.add("is-hidden");
+			break;
+		case "page-2.html":
+			const loader2 = document.getElementById("loader");
+			loader2.classList.add("is-hidden-left");
+			break;
+	}
+	title.textContent += " Bienvenue sur " + page;
 }
 
 window.addEventListener("DOMContentLoaded", loading);
