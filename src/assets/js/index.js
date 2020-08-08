b231457d5a9d736dc9679f65b4bfc49b2e83e690
Devidev <47260520+dev634@users.noreply.github.com>
2020-08-09 01:00:35 +0200

    sec commit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,16 +1,23 @@
 function loading() {
 	const title = document.querySelector("main h2");
-
+	let loader = "";
 	const path = window.location.pathname;
 	const page = path.split("/").pop();
 	switch (page) {
+		case "":
+			loader = document.getElementById("loader");
+			loader.classList.add("is-hidden");
+			break;
 		case "index.html":
-			const loader = document.getElementById("loader");
+			loader = document.getElementById("loader");
 			loader.classList.add("is-hidden");
 			break;
 		case "page-2.html":
-			const loader2 = document.getElementById("loader");
-			loader2.classList.add("is-hidden-left");
+			loader = document.getElementById("loader");
+			loader.classList.add("is-hidden-left");
+			break;
+		default:
+			alert("something wrong ....");
 			break;
 	}
 	title.textContent += " Bienvenue sur " + page;
